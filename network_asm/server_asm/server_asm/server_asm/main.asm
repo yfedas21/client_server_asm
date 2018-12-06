db3296670cb63664e054838df56429ba55a7d3bb
Yuriy V Fedas <yfedas21@my.whitworth.edu>
2018-12-06 00:18:22 -0800

    Finished writing program logic, added all function prototypes, added most constants...

--- network_asm/server_asm/server_asm/server_asm/main.asm
+++ network_asm/server_asm/server_asm/server_asm/main.asm
@@ -20,6 +20,17 @@
 ;			a. Use localhost (127.0.0.1:5400)
 ;		4. Tell winsock the socket is a listening socket (call listen)
 ;		5. Wait for a client connection (call accept)
+;		6. After client connects, get client info
+;			a. client hostname
+;			b. service name
+;			c. display host info to console
+;		7. Close the listening socket (call closesocket)
+;		8. While-loop to accept data from client (as long as they send)
+;		9. Receive data from client (call recv)
+;		10. If the client sends any data, echo it back to them
+;		11. After the client disconnects, close the client socket (call closesocket)
+;		12.	Clean up winsock
+;
 ; $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
 .686P
@@ -29,6 +40,7 @@
 INCLUDE Irvine32.inc			; for output, Irvine functions
 
 INCLUDELIB WS2_32				; For WinAPI functions 
+INCLUDELIB UCRT					; For memset 
 
 EXTRN	accept@12:PROC
 EXTRN	bind@12:PROC
@@ -43,6 +55,7 @@ EXTRN	WSAStartup@8:PROC
 EXTRN	WSACleanup@0:PROC
 EXTRN	getnameinfo@28:PROC
 EXTRN	inet_ntop@16:PROC
+EXTRN	memset:PROC
 
 .data
 
@@ -59,7 +72,9 @@ NI_MAXSERV				= 32	; max size of a service name
 INVALID_SOCKET			= -1	; the 2s comp of 0 (~0), because SOCKET is unsigned
 SOCKET_ERROR			= -1	; synonymous to INVALID_SOCKET in this context
 SOMAXCONN				= 7fffffffh		; maxinmum # of pending connections in queue
-; ---------------------------------------------------------------------------------------
+
+; --------------------------------- my constants ----------------------------------------
+BUF_SIZE				= 4096	; the size of the buffer client sends data into
 ; ***************************************************************************************
 
 errStr BYTE "Hello World from Assembly",0
